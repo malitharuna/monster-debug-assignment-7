a2def13d1f5b5b4d05167d1c3ac181e8b43c4e74
malitharuna <msmalitharuna@gmail.com>
2022-09-15 18:58:33 +0600

    fnished

--- script.js
+++ script.js
@@ -110,9 +110,10 @@ const start = () => {
 
   const startCountdown = setInterval(() => {
     countdownOverlay.innerHTML = `<h1>${count}</h1>`;
-
+   
     // finished timer
-    if (count < 0) {
+    if (count == 0 ) {
+      
       // -------------- START TYPING -----------------
       document.addEventListener("keydown", typeController);
       countdownOverlay.style.display = "none";
@@ -120,9 +121,11 @@ const start = () => {
 
       clearInterval(startCountdown);
       startTime = new Date().getTime();
+      
     }
     count--;
   }, 1000);
+  countdownOverlay.innerHTML = "";
 };
 
 // START Countdown
